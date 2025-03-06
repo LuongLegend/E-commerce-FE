6e37b3f0ec690ad13772c9afa312ec2166999768
luonglegend <luongprolv99@gmail.com>
2025-03-06 22:17:49 +0700

    link to naviation

--- src/layouts/Client/NavigationBar/NavigationBar.tsx
+++ src/layouts/Client/NavigationBar/NavigationBar.tsx
@@ -1,26 +1,63 @@
+import { Button, Flex, Popover } from 'antd'
 import { CustomerServiceOutlined, AppstoreOutlined } from '@ant-design/icons'
-import { Button, Flex } from 'antd'
-import CustomCascader from '../../../components/CustomCascader'
+import { Link } from 'react-router'
+
 import './NavigationBar.css'
+
+interface Option {
+  value: string
+  label: string
+  children?: Option[]
+}
 
 const shopOptions = [
   {
-    value: 'zhejiang',
-    label: 'Zhejiang'
+    value: 'zhejiang1',
+    label: 'Zhejiang1'
   },
   {
-    value: 'zhejiang',
-    label: 'Zhejiang'
+    value: 'zhejiang2',
+    label: 'Zhejiang2'
   },
   {
-    value: 'zhejiang',
-    label: 'Zhejiang'
+    value: 'zhejiang3',
+    label: 'Zhejiang3'
   },
   {
-    value: 'zhejiang',
-    label: 'Zhejiang'
+    value: 'zhejiang4',
+    label: 'Zhejiang4'
   }
 ]
+
+const venderOptions = [
+  {
+    value: 'zhejiang1',
+    label: 'Zhejiang1'
+  },
+  {
+    value: 'zhejiang2',
+    label: 'Zhejiang2'
+  },
+  {
+    value: 'zhejiang3',
+    label: 'Zhejiang3'
+  },
+  {
+    value: 'zhejiang4',
+    label: 'Zhejiang4'
+  }
+]
+
+const optionsContent = (options: Option[]) => (
+  <Flex vertical style={{ width: 200 }} gap={'middle'}>
+    {options.map(({ value, label }) => (
+      <Link to={`/shop/${value}`} key={value} className={'option-link bold-text'}>
+        {label}
+      </Link>
+    ))}
+  </Flex>
+)
+
 const NavigationBar = () => {
   return (
     <Flex gap={'large'}>
@@ -29,32 +66,44 @@ const NavigationBar = () => {
         All categories
       </Button>
       <Flex justify='center' gap={'large'} style={{ flexGrow: 6 }}>
-        <Button color='primary' size='large' variant='text' className='bold-text'>
+        <Button color='primary' size='large' variant='text' className='bold-text '>
           Hot deals
         </Button>
-        <Button color='primary' size='large' variant='text' className='bold-text'>
-          About
-        </Button>
-        <Button color='primary' size='large' variant='text' className='bold-text'>
-          Shop
-        </Button>
-        <Button color='primary' size='large' variant='text' className='bold-text'>
-          Vendors
-        </Button>
+        <Link to={'/about'}>
+          <Button color='primary' size='large' variant='text' className='bold-text'>
+            About
+          </Button>
+        </Link>
+        <Popover content={optionsContent(shopOptions)} arrow={false} placement='bottomLeft'>
+          <Button color='primary' size='large' variant='text' className='bold-text'>
+            Shop
+          </Button>
+        </Popover>
+        <Popover content={optionsContent(venderOptions)} arrow={false} placement='bottomLeft'>
+          <Button color='primary' size='large' variant='text' className='bold-text'>
+            Vendors
+          </Button>
+        </Popover>
         <Button color='primary' size='large' variant='text' className='bold-text'>
           Mega menu
         </Button>
-        <Button color='primary' size='large' variant='text' className='bold-text'>
-          Blogs
-        </Button>
-        <Button color='primary' size='large' variant='text' className='bold-text'>
-          Contact
-        </Button>
+        <Link to={'/blogs'}>
+          <Button color='primary' size='large' variant='text' className='bold-text'>
+            Blogs
+          </Button>
+        </Link>
+        <Link to={'/contact'}>
+          <Button color='primary' size='large' variant='text' className='bold-text'>
+            Contact
+          </Button>
+        </Link>
       </Flex>
       <Flex gap={'small'}>
         <CustomerServiceOutlined style={{ fontSize: 40 }} />
         <Flex vertical>
-          <span className='phone-contact'>1900-888 </span>
+          <a href='tel:1900-888' className='phone-contact'>
+            1900-888
+          </a>
           <span className='support-center'>24/7 Support center</span>
         </Flex>
       </Flex>
