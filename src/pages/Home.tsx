037b22aa5f0a8261932dc4593edf3e6075e775df
luonglegend <luongprolv99@gmail.com>
2025-03-14 10:42:23 +0700

    list product

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react'
+import callApi from '../utils/callApi'
+import Products from './Products'
+
 const Home = () => {
-  return <>Home</>
+  const [products, setProducts] = useState([])
+  const getProducts = async () => {
+    const result = await callApi('/product')
+    const { data, status } = result
+    if (status === 1) {
+      setProducts(data)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  return (
+    <>
+      <Products products={products} />
+    </>
+  )
 }
 
 export default Home
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,8 +2,6 @@ import { Flex, Typography, Form, Input, Checkbox, Button, Space, message } from 
 import { Link, useNavigate } from 'react-router'
 
 import callApi from '../utils/callApi'
-import { useContext } from 'react'
-import { AppContext } from '../App'
 
 type RegisterFormType = {
   username: string
@@ -16,7 +14,6 @@ type RegisterFormType = {
 const Register = () => {
   const [form] = Form.useForm()
   const [messageApi, contextHolder] = message.useMessage()
-  const appContext = useContext(AppContext)
   const navigate = useNavigate()
   const { Title } = Typography
 
