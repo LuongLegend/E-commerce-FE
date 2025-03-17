2d52066decfb25d597d3a869b4057f397220a6d6
luonglegend <luongprolv99@gmail.com>
2025-03-17 14:10:25 +0700

    implement redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
 import { ConfigProvider } from 'antd'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-import { store } from './store/store'
+import { store, persistor } from './store/store'
 
 import ClientLayout from './layouts/Client/index'
 import Home from './pages/Home'
@@ -20,30 +21,32 @@ function App({ children }: React.PropsWithChildren) {
   return (
     <>
       <Provider store={store}>
-        <AntdRegistry>
-          <ConfigProvider
-            theme={{
-              token: {
-                colorPrimary: '#3bb77e',
-                fontSize: 16
-              }
-            }}
-          >
-            <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_APP}>
-              {children}
-              <Routes>
-                <Route element={<ClientLayout />}>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/login' element={<Login />} />
-                  <Route path='/register' element={<Register />} />
-                  <Route element={<ProtectedRoute />}>
-                    <Route path='/account' element={<Account />} />
+        <PersistGate loading={null} persistor={persistor}>
+          <AntdRegistry>
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorPrimary: '#3bb77e',
+                  fontSize: 16
+                }
+              }}
+            >
+              <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_APP}>
+                {children}
+                <Routes>
+                  <Route element={<ClientLayout />}>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route element={<ProtectedRoute />}>
+                      <Route path='/account' element={<Account />} />
+                    </Route>
                   </Route>
-                </Route>
-              </Routes>
-            </GoogleOAuthProvider>
-          </ConfigProvider>
-        </AntdRegistry>
+                </Routes>
+              </GoogleOAuthProvider>
+            </ConfigProvider>
+          </AntdRegistry>
+        </PersistGate>
       </Provider>
     </>
   )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import userReducer from './slices/userSlice'
 
-export const store = configureStore({
-  reducer: { user: userReducer }
+const persistConfig = {
+  key: 'root',
+  storage
+}
+const reducer = combineReducers({
+  user: userReducer
 })
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+      }
+    })
+})
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
